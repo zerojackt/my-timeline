07b593bc68ed9939bdd47ba3a52d3ebb6e74e739
zero jack <zerojackk1002@gmail.com>
2020-01-11 15:56:11 +0900

    start

---
+++ src/main/java/net/timeline/MyTimelineApplication.java
@@ -0,0 +1,13 @@
+package net.timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTimelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/timeline/MyTimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package net.timeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
